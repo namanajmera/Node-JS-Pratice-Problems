ad7d4f9931807c44b5e002d70366ea12121f4e3f
Naman <cool.naman.ajmera@gmail.com>
2021-05-24 17:54:13 +0530

    Added: Use Random to get Dice number from 1 to 6

--- Sequence Pratice Problems/REPLProblems.js
+++ Sequence Pratice Problems/REPLProblems.js
@@ -2,3 +2,8 @@
 
 let digit = Math.floor(Math.random() * 10);
 console.log("Single Digit Value is:- " + digit)
+
+
+// Use Random to get Dice Number between 1 to 6
+let dice = Math.floor(Math.random() * 10) % 6 + 1
+console.log("Random dice value is:- " + dice)
