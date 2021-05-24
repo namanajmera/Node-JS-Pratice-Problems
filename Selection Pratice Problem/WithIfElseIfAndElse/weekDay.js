72de01ae09bd4092f4a50d4b6f7908edc9f8fbfa
Naman <cool.naman.ajmera@gmail.com>
2021-05-24 21:14:05 +0530

    Added: Find the week day name.

---
+++ Selection Pratice Problem/WithIfElseIfAndElse/weekDay.js
@@ -0,0 +1,20 @@
+const prompt = require('prompt-sync')();
+
+var value = prompt("Enter the number 1-7 to print the week day:- ")
+
+if (value == 1)
+    console.log("Sunday")
+else if (value == 2)
+    console.log("Monday")
+else if (value == 3)
+    console.log("Tuesday")
+else if (value == 4)
+    console.log("Wednesday")
+else if (value == 5)
+    console.log("Thursday")
+else if (value == 6)
+    console.log("Friday")
+else if (value == 7)
+    console.log("Saturday")
+else
+    console.log("Enter the wright number.")
