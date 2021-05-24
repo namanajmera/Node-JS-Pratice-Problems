29b6e182f82463e2b4f9df27888bd72787593f2e
Naman <cool.naman.ajmera@gmail.com>
2021-05-24 17:57:51 +0530

    Added: Add two Random Dice Number and Print the Result

--- Sequence Pratice Problems/REPLProblems.js
+++ Sequence Pratice Problems/REPLProblems.js
@@ -3,7 +3,11 @@
 let digit = Math.floor(Math.random() * 10);
 console.log("Single Digit Value is:- " + digit)
 
-
 // Use Random to get Dice Number between 1 to 6
 let dice = Math.floor(Math.random() * 10) % 6 + 1
 console.log("Random dice value is:- " + dice)
+
+// Add two Random Dice Number and Print the Result
+let dice1 = Math.floor(Math.random() * 10) % 6 + 1
+let dice2 = Math.floor(Math.random() * 10) % 6 + 1
+console.log("Addition of two random dice is:- " + (dice1 + dice2))
