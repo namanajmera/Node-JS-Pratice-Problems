7d1362e8d715935f1b8f677d4ec7b5a1ba9e41e8
Naman <cool.naman.ajmera@gmail.com>
2021-05-24 18:46:35 +0530

    Added: Converting the inches into feet

---
+++ Sequence Pratice Problems/UnitConversion.js
@@ -0,0 +1,6 @@
+// Converting the inches into feet
+const CONVERSION_FACTOR_OF_INCH_INTO_FEET = 1 / 12
+
+let inches = 42
+let feet = inches * CONVERSION_FACTOR_OF_INCH_INTO_FEET
+console.log(inches + " in = " + feet + " ft.")
